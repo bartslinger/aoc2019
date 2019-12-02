f3a1a3480d57cd0f55427ec6ec5b056993ba1688
Bart Slinger <bartslinger@gmail.com>
2019-12-02 10:42:27 +0100

    day 2: remove mutable references, pass ownership instead

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -6,54 +6,39 @@ mod tests {
 
     #[test]
     fn test_run_program() {
-
-        let mut program: Vec<usize> = vec![1,0,0,0,99];
-        run_program(&mut program);
-        assert_eq!(vec![2,0,0,0,99], program);
-
-        let mut program: Vec<usize> = vec![2,3,0,3,99];
-        run_program(&mut program);
-        assert_eq!(vec![2,3,0,6,99], program);
-
-        let mut program: Vec<usize> = vec![2,4,4,5,99,0];
-        run_program(&mut program);
-        assert_eq!(vec![2,4,4,5,99,9801], program);
-
-        let mut program: Vec<usize> = vec![1,1,1,4,99,5,6,0,99];
-        run_program(&mut program);
-        assert_eq!(vec![30,1,1,4,2,5,6,0,99], program);
+        assert_eq!(vec![2,0,0,0,99], run_program(vec![1,0,0,0,99]));
+        assert_eq!(vec![2,3,0,6,99], run_program(vec![2,3,0,3,99]));
+        assert_eq!(vec![2,4,4,5,99,9801], run_program(vec![2,4,4,5,99,0]));
+        assert_eq!(vec![30,1,1,4,2,5,6,0,99], run_program(vec![1,1,1,4,99,5,6,0,99]));
     }
 }
 
-fn run_program(program: &mut Vec<usize>) {
+fn run_program(mut program: Vec<usize>) -> Vec<usize> {
     let mut i = 0;
     loop {
         match program[i] {
             1 => {
-                let first_i = program[i+1];
-                let second_i = program[i+2];
                 let mut_i = program[i+3];
-                program[mut_i] = program[first_i] + program[second_i];
+                program[mut_i] = program[program[i+1]] + program[program[i+2]];
             },
             2 => {
-                let first_i = program[i+1];
-                let second_i = program[i+2];
                 let mut_i = program[i+3];
-                program[mut_i] = program[first_i] * program[second_i];
+                program[mut_i] = program[program[i+1]] * program[program[i+2]];
             },
             99 => break,
             _ => panic!("Invalid opcode")
         }
         i += 4;
     }
+    program
 }
 
 fn part_one(original_program: &Vec<usize>) -> usize {
     let mut program = original_program.clone();
     program[1] = 12;
     program[2] = 2;
-    run_program(&mut program);
-    program[0]
+    let output = run_program(program);
+    output[0]
 }
 
 fn part_two(original_program: &Vec<usize>, goal: usize) -> usize {
@@ -62,8 +47,8 @@ fn part_two(original_program: &Vec<usize>, goal: usize) -> usize {
             let mut program = original_program.clone();
             program[1] = i;
             program[2] = j;
-            run_program(&mut program);
-            if program[0] == goal {
+            let output = run_program(program);
+            if output[0] == goal {
                 return 100 * i + j;
             }
         }
