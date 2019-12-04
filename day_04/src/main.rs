1e2db4e44b9e634db3108cb0aad672fa6e8efd8e
Bart Slinger <bartslinger@gmail.com>
2019-12-04 22:04:34 +0100

    day 4 refactor and actually produce both outputs

--- day_04/src/main.rs
+++ day_04/src/main.rs
@@ -5,14 +5,29 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_valid_numbers() {
-        assert!(valid(111111));
-        assert!(!valid(223450));
-        assert!(!valid(123789));
+    fn test_valid_numbers_one() {
+        assert!(valid(111111).0);
+        assert!(!valid(223450).0);
+        assert!(!valid(123789).0);
+    }
+
+    #[test]
+    fn test_valid_numbers_two() {
+        assert!(valid(112233).1);
+        assert!(!valid(123444).1);
+        assert!(valid(111122).1);
+    }
+
+    #[test]
+    fn test_find_valid_options() {
+        // using puzzle answers
+        let answers = find_valid_options(246540, 787419);
+        assert_eq!(1063, answers.0);
+        assert_eq!(686, answers.1);
     }
 }
 
-fn valid(number: u64) -> bool {
+fn valid(number: u32) -> (bool, bool) {
     let n = format!("{}", number);
     assert_eq!(6, n.len());
 
@@ -24,7 +39,7 @@ fn valid(number: u64) -> bool {
         b[2] >= b[1] &&
         b[1] >= b[0])
     {
-        return false;
+        return (false, false);
     }
 
     if b[5] == b[4] ||
@@ -33,10 +48,10 @@ fn valid(number: u64) -> bool {
         b[2] == b[1] ||
         b[1] == b[0]
     {
-        return has_double(b);
+        return (true, has_double(b));
     }
 
-    return false;
+    return (false, false);
 }
 
 fn has_double(b: &[u8]) -> bool {
@@ -58,20 +73,32 @@ fn has_double(b: &[u8]) -> bool {
     false
 }
 
-fn main() {
-    let contents = fs::read_to_string("data/input").unwrap();
-
-    let numbers = contents.trim().split('-').map(|x| x.parse::<u64>().unwrap()).collect::<Vec<_>>();
+fn find_valid_options(start: u32, end: u32) -> (u32, u32) {
 
     // Brute force!
     let mut cnt = 0;
-    for number in numbers[0]..=numbers[1] {
-        if valid(number) {
+    let mut cnt2 = 0;
+    for number in start..=end {
+        let r = valid(number);
+        if r.0 {
             cnt += 1;
+        }
+        if r.1 {
+            cnt2 += 1;
         }
     }
 
-    println!("Part 1: {}", cnt);
+    (cnt,cnt2)
+}
+
+fn main() {
+    let contents = fs::read_to_string("data/input").unwrap();
+
+    let numbers = contents.trim().split('-').map(|x| x.parse::<u32>().unwrap()).collect::<Vec<_>>();
+
+    let answers = find_valid_options(numbers[0], numbers[1]);
+    println!("Part 1: {}", answers.0);
+    println!("Part 2: {}", answers.1);
 
 
 }
