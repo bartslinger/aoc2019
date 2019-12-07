46ae2d2522f2a748426b6690d36d0801233cc24e
Bart Slinger <bartslinger@gmail.com>
2019-12-07 01:09:57 +0100

    day 5 add test to make sure it keeps working

--- day_05/src/main.rs
+++ day_05/src/main.rs
@@ -1,14 +1,33 @@
 use aoc;
 
+#[cfg(test)]
+mod tests {
+    use super::{part_one, part_two};
+
+    #[test]
+    fn test_puzzle_answer() {
+        let original_program = aoc::vector_from_comma_separated_file::<i64>("data/input")
+        .expect("Error getting program");
+        assert_eq!(13787043, part_one(original_program.clone()));
+        assert_eq!(3892695, part_two(original_program.clone()));
+    }
+
+}
+
+fn part_one(program: Vec<i64>) -> i64 {
+    let output = aoc::intcode_computer::run_program(program, vec![1], vec![]);
+    *output.2.last().unwrap()
+}
+
+fn part_two(program: Vec<i64>) -> i64 {
+    let output = aoc::intcode_computer::run_program(program, vec![5], vec![]);
+    *output.2.last().unwrap()
+}
+
 fn main() {
     let original_program = aoc::vector_from_comma_separated_file::<i64>("data/input")
         .expect("Error getting program");
 
-    let part_one = aoc::intcode_computer::run_program(original_program.clone(), vec![1], vec![]);
-    let part_two = aoc::intcode_computer::run_program(original_program.clone(), vec![5], vec![]);
-
-    println!("Part 1: {:?}", part_one.2);
-    println!("Part 2: {:?}", part_two.2);
-
-
+    println!("Part 1: {:?}", part_one(original_program.clone()));
+    println!("Part 2: {:?}", part_two(original_program.clone()));
 }
