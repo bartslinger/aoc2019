b8490418376b4aadbfc211163496c34fb69435e3
Bart Slinger <bartslinger@gmail.com>
2019-12-07 12:38:39 +0100

    day 6, refactor part 1 and do part 2

--- day_06/src/main.rs
+++ day_06/src/main.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 
 #[cfg(test)]
 mod tests {
-    use super::{get_orbit_count, get_orbits_vector};
+    use super::{get_orbits_vector, build_galaxy_map, count_all_orbits, get_distance_to_santa};
 
     #[test]
     fn test_orbit_count() {
@@ -20,37 +20,80 @@ mod tests {
         E)J
         K)L";
         let orbits = get_orbits_vector(input);
-        let orbit_count = get_orbit_count(orbits);
+        let galaxy_map = build_galaxy_map(orbits);
+        let orbit_count = count_all_orbits(&galaxy_map);
         assert_eq!(42, orbit_count);
+    }
+
+    #[test]
+    fn test_get_distance_to_santa() {
+        let input = "COM)B
+        B)C
+        C)D
+        D)E
+        E)F
+        B)G
+        G)H
+        D)I
+        E)J
+        J)K
+        K)L
+        K)YOU
+        I)SAN";
+        let orbits = get_orbits_vector(input);
+        let galaxy_map = build_galaxy_map(orbits);
+
+        assert_eq!(4, get_distance_to_santa(galaxy_map));
     }
 }
 
-fn get_orbit_count(mut orbits: Vec<Vec<&str>>) -> usize {
-
-    let mut galaxy = HashMap::new();
-    galaxy.insert("COM", 0);
-
-    let mut sum = 0;
-    while orbits.len() > 0 {
-        let mut new_todo = Vec::new();
-
-        for orbit in &orbits {
-            match galaxy.get(orbit[0]) {
-                Some(v) => {
-                    let val = v + 1;
-                    galaxy.insert(orbit[1], val);
-                    sum += val;
-                },
-                None => {
-                    new_todo.push(orbit.clone());
-                }
-            };
-        }
-
-        orbits = new_todo;
+fn get_distance_to_santa(galaxy_map: HashMap<&str,&str>) -> usize {
+    // First, follow the trajectory of santa to COM
+    let mut santa_orbit = HashMap::new();
+    let mut next: &str = galaxy_map.get("SAN").unwrap();
+    let mut dist: usize = 0;
+    while next != "COM" {
+        santa_orbit.insert(next, dist);
+        dist += 1;
+        next = galaxy_map.get(next).unwrap();
     }
 
+    // Now follow trajectory from YOU until there is a match
+    let mut next: &str = galaxy_map.get("YOU").unwrap();
+    let mut dist = 0;
+    loop {
+        match santa_orbit.get(next) {
+            Some(v) => return v + dist,
+            None => ()
+        }
+        dist += 1;
+        next = galaxy_map.get(next).unwrap();
+    }
+}
+
+fn count_all_orbits(galaxy_map: &HashMap<&str, &str>) -> usize {
+    let mut sum = 0;
+    for orbit in galaxy_map {
+        // keep counting till COM is reached
+        let mut next: &str = orbit.1;
+        while next != "COM" {
+            sum += 1;
+            next = galaxy_map.get(next).unwrap();
+        }
+        sum += 1; // Count orbit around COM
+    }
     sum
+}
+
+fn build_galaxy_map(orbits: Vec<Vec<&str>>) -> HashMap<&str, &str> {
+
+    let mut galaxy = HashMap::new();
+
+    for orbit in orbits {
+        galaxy.insert(orbit[1], orbit[0]);
+    }
+
+    galaxy
 }
 
 fn get_orbits_vector(raw_input: &str) -> Vec<Vec<&str>> {
@@ -62,10 +105,11 @@ fn get_orbits_vector(raw_input: &str) -> Vec<Vec<&str>> {
     orbits
 }
 
-
 fn main() {
     let contents = fs::read_to_string("data/input").unwrap();
-    let orbits_vector = get_orbits_vector(&contents);
+    let orbits = get_orbits_vector(&contents);
+    let galaxy_map = build_galaxy_map(orbits.clone());
 
-    println!("Part 1: {}", get_orbit_count(orbits_vector));
+    println!("Part 1: {}", count_all_orbits(&galaxy_map));
+    println!("Part 2: {}", get_distance_to_santa(galaxy_map));
 }
