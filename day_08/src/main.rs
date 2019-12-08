939883bce2b9d1210cb04498db98a6d67ba2bb5b
Bart Slinger <bartslinger@gmail.com>
2019-12-08 14:42:17 +0100

    day 8 more iterators and increased readability

--- day_08/src/main.rs
+++ day_08/src/main.rs
@@ -14,18 +14,29 @@ mod tests {
 fn part_one(input: &str, width: usize, height: usize) -> usize {
     let size = width * height;
     let charvec: Vec<char> = input.chars().collect();
-    let layers: Vec<&[char]> = charvec.chunks(size).collect();
-    let layer_with_least_zeros = layers.iter().min_by_key(|layer| layer.iter().filter(|&ch| *ch=='0').count()).unwrap();
+    let layer_with_least_zeros = charvec.chunks(size)
+        .min_by_key(|layer|
+            layer.iter()
+            .filter(|&ch| *ch=='0')
+            .count()
+        )
+        .unwrap();
 
-    ['1', '2'].iter().map(|s| layer_with_least_zeros.iter().filter(|&c| *c==*s).count()).product()
+    ['1', '2'].iter()
+        .map(|s|
+            layer_with_least_zeros.iter()
+            .filter(|&c| *c==*s)
+            .count()
+        )
+        .product()
 }
 
 fn part_two(input: &str, width: usize, height: usize) {
     let size = width * height;
-
     let charvec: Vec<char> = input.chars().collect();
     let layers: Vec<&[char]> = charvec.chunks(size).collect();
 
+    // Closure to produce new pixel value
     let pixel_value = |x:(&char,&char)| -> char {
         if *x.0=='2' {
             match *x.1 {
@@ -38,15 +49,20 @@ fn part_two(input: &str, width: usize, height: usize) {
         }
     };
 
-    let mut image: Vec<char> = vec!['2'; size];
-    for layer in layers {
-        // Zip the two iterators to produce single output
-        image = image.iter().zip(layer).map(|x| pixel_value(x)).collect();
-    }
+    let image: Vec<char> = layers.iter().fold(
+        vec!['2'; size],
+        |im, layer| im.iter()
+            .zip(layer.iter())
+            .map(|x| pixel_value(x))
+            .collect()
+        );
 
-    for line in image.chunks(width) {
-        println!("{}", line.iter().collect::<String>());
-    }
+    // Visualize as string
+    let output = image.chunks(width)
+        .map(|row| row.iter().collect::<String>())
+        .collect::<Vec<String>>()
+        .join("\n");
+    println!("{}", output);
 }
 
 fn main() {
