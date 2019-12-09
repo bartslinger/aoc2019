6cb7d92d89fcef0ff419665ac5d05e3a4c22d7cc
Bart Slinger <bartslinger@gmail.com>
2019-12-09 23:39:27 +0100

    day 9 part 2 beuned

--- day_09/src/main.rs
+++ day_09/src/main.rs
@@ -7,6 +7,8 @@ fn main() {
     let original_program_state = aoc::intcode_computer::ProgramState {mem: aoc::intcode_computer::extended(original_program), pc: 0, rb: 0, halted: false};
     let result = aoc::intcode_computer::run_program(original_program_state.clone(), vec![1], vec![]);
 
-    println!("Part 1: {:}", result.2[0]);
+    println!("Part 1: {}", result.2[0]);
 
+    let result = aoc::intcode_computer::run_program(original_program_state.clone(), vec![2], vec![]);
+    println!("Part 2: {}", result.2[0]);
 }
